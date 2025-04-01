fb988bfd1a01e6f00ea2b0bd1082f19fab9f1df0
Jean <jeanikkt@gmail.com>
2025-03-31 23:37:19 -0300

    saida de dados

--- Ex_2_1.c
+++ Ex_2_1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main(){
-	#ENTRADA DE DADOS
-	printf("Oi, tudo bem? Tenho 6 anos e programo.\n");
-	
-	printf("Valor inteiro: %d. \n", 10);
-	printf("Valor real: %f. \n", 3.14159265);
-	printf("Valor real com apenas duas casas: %.2f. \n", 3.14159265);
-	printf("Dado de texto: %c. \n", 'a');
-	printf("Dado de texto: %s. \n", "testando");
-	#SAIDA DE DADOS
+//	ENTRADA DE DADOS
+//	printf("Oi, tudo bem? Tenho 6 anos e programo.\n");
+//	
+//	printf("Valor inteiro: %d. \n", 10);
+//	printf("Valor real: %f. \n", 3.14159265);
+//	printf("Valor real com apenas duas casas: %.2f. \n", 3.14159265);
+//	printf("Dado de texto: %c. \n", 'a');
+//	printf("Dado de texto: %s. \n", "testando");
+//	SAIDA DE DADOS
 	
 	int idade = 0;
 	
