036eaf567c33a919f1f4d79962e4e01d67c3f01f
Jean <jeanikkt@gmail.com>
2025-03-31 23:21:17 -0300

    saida de dados

--- Ex_2_1.c
+++ Ex_2_1.c
@@ -10,4 +10,13 @@ int main(){
 	printf("Dado de texto: %c. \n", 'a');
 	printf("Dado de texto: %s. \n", "testando");
 	#SAIDA DE DADOS
+	
+	int idade = 0;
+	
+	printf("Valor inicial da idade: %d. \n", idade);
+	
+	printf("Digite uma idade:\n");
+	scanf("%d", &idade);
+	
+	printf("Idade informada: %d. \n", idade);
 }
