457dbd781d42b13744d5d1ee76e91e59ffa3afa9
Jean <jeanikkt@gmail.com>
2025-05-06 02:23:42 -0300

    update: variaveis

--- variaveis.c
+++ variaveis.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
+#define texto "Entrada e saida de dados"
 
-int main(){
 	//VARIAVEIS 
+	
+	//Espaço em memoria para armazenar dados
+	
+	//	->SINTAXE declaração: <tipo> <nome>;
+	//	
+	//	-> NOMENCLATURA:
+	//		|-> Não pode número na 1 posição
+	//		|-> não pode caracteres especiais
+	//		|-> não pode espaço
+	//
+	
+int main(){
+	printf("%s\n",texto);
+	
+	int idade = 0;
+	float altura = 0.0;
+	char nome[50] = "";
+	
+	printf("Digite a idade:\n");
+	scanf("%d", &idade);
+	
+	printf("Digite a altura:\n");
+	scanf("%f", &altura);
+	
+	printf("Digite o nome:\n");
+	scanf("%s", &nome);
+	
+	
 }
