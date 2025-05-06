fc3525b66f1fa73e92bf66d0ed6621a4d3f2409e
Jean <jeanikkt@gmail.com>
2025-05-06 02:38:43 -0300

    update: calculos em C

--- variaveis.c
+++ variaveis.c
@@ -14,25 +14,51 @@
 	//		|-> não pode espaço
 	//
 	
+//	* Operadores matemáticos:
+//		|-> Soma: +
+//		|-> Subtração: -
+//		|-> Multiplicação: *
+//		|-> Divisão: /
+//		|-> Resto de divisão inteira: %
+//	* Existe precedência entre os operadores!
+	
 int main(){
-	printf("%s\n",texto);
 	
-	int idade = 0;
-	float altura = 0.0;
-	char nome[50] = "";
+//	printf("%s\n",texto);
+//	
+//	int idade = 0;
+//	float altura = 0.0;
+//	char nome[50] = "";
+//	
+//	printf("Digite a idade:\n");
+//	scanf("%d", &idade);
+//	
+//	printf("Digite a altura:\n");
+//	scanf("%f", &altura);
+//	
+//	printf("Digite o nome:\n");
+//	scanf("%s", &nome);
+//	
+//	printf("Dados Informados:\n");
+//	printf("Idade: %d.\n", idade);
+//	printf("Altura: %.2f.\n", altura);
+//	printf("Nome: %s.\n", nome);
 	
-	printf("Digite a idade:\n");
-	scanf("%d", &idade);
+	int A, B, soma, subtr, mult, divis;
 	
-	printf("Digite a altura:\n");
-	scanf("%f", &altura);
+	printf("Digite o primeiro valor:\n");
+	scanf("%d", &A);
+	printf("Digite o segundo valor:\n");
+	scanf("%d", &B);
 	
-	printf("Digite o nome:\n");
-	scanf("%s", &nome);
+	soma = A + B;
+	subtr = A - B;
+	mult = A * B;
+	divis = A / B;
 	
-	printf("Dados Informados:\n");
-	printf("Idade: %d.\n", idade);
-	printf("Altura: %.2f.\n", altura);
-	printf("Nome: %s.\n", nome);
-	
+	printf("Resultado:\n");
+	printf("Soma: %d.\n", soma);
+	printf("Subtra: %d.\n", subtr);
+	printf("Multiplic: %d.\n", mult);
+	printf("Divisao: %d.\n", divis);
 }
